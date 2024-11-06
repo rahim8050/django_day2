0da5ce4c8ca9cfdd25979f0f40ffd034b5564c9a
Shallon <shallonmaria2001@gmail.com>
2024-11-06 10:20:26 +0200

    Data display, loops, if statements, templating engine language

--- main_app/views.py
+++ main_app/views.py
@@ -7,8 +7,22 @@ def home(request):
 
 
 def services(request):
-    return render(request, 'services.html')
+    our_services = ["Bush Camps", "Balloon Tours", "Hunting", "Village Visiing", "Yoga"]
+    price = 10000
+    date = '15-11-2024'
+    return render(request, 'services.html', {"services": our_services, "price": price, "date": date})
 
 
 def about(request):
     return render(request, 'about.html')
+
+# display data in our pages
+# loops
+# if statements
+# templating engine language
+
+# filters
+# images
+# static files
+# database
+# install xammp
